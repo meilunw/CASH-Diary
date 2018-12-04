53a008a4c004324a0e7214c440487eeaf19459a1
meilunw <meilunw2@illinois.edu>
2018-12-04 11:29:05 -0600

    adding click-in activity

---
+++ app/src/main/java/com/example/wumeilun/cashdiary/Balance_activity.java
@@ -0,0 +1,13 @@
+package com.example.wumeilun.cashdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Balance_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_activity);
+    }
+}
---
+++ app/src/main/java/com/example/wumeilun/cashdiary/Expense_activity.java
@@ -0,0 +1,13 @@
+package com.example.wumeilun.cashdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Expense_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_activity);
+    }
+}
