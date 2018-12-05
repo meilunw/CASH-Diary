780f8f680aeebde69b7c25a145043c5ee4ef2ef8
meilunw <meilunw2@illinois.edu>
2018-12-05 13:26:10 -0600

    added page

---
+++ app/src/main/java/com/example/wumeilun/cashdiary/AddedIncome.java
@@ -0,0 +1,13 @@
+package com.example.wumeilun.cashdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddedIncome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_added_income);
+    }
+}
--- app/src/main/java/com/example/wumeilun/cashdiary/Income_activity.java
+++ app/src/main/java/com/example/wumeilun/cashdiary/Income_activity.java
@@ -1,13 +1,28 @@
 package com.example.wumeilun.cashdiary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Income_activity extends AppCompatActivity {
+    Button AddIncome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income_activity);
+        AddIncome = findViewById(R.id.Add_New_Income);
+        AddIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addincome();
+            }
+        });
+    }
+    public void addincome() {
+        Intent intent = new Intent(this, AddedIncome.class);
+        startActivity(intent);
     }
 }
