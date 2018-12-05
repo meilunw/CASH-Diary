19bd9fd476d6181715ddbe02ce1e7f185cfff78e
meilunw <meilunw2@illinois.edu>
2018-12-05 13:55:30 -0600

    added expense

---
+++ app/src/main/java/com/example/wumeilun/cashdiary/Added_Expense.java
@@ -0,0 +1,13 @@
+package com.example.wumeilun.cashdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Added_Expense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_added__expense);
+    }
+}
--- app/src/main/java/com/example/wumeilun/cashdiary/Expense_activity.java
+++ app/src/main/java/com/example/wumeilun/cashdiary/Expense_activity.java
@@ -2,12 +2,27 @@ package com.example.wumeilun.cashdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class Expense_activity extends AppCompatActivity {
-
+    Button addedExpense;
+    Button canceledExpense;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_activity);
+        addedExpense= findViewById(R.id.Add_New_Expense);
+        addedExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addedExpense();
+            }
+        });
+    }
+    public void addedExpense() {
+        Intent intent = new Intent(this, Added_Expense.class);
+        startActivity(intent);
     }
 }
